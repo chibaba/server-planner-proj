3b31bee0d814ae05f2d01a22db9602ec0b63d650
chi <chinedu.onyeoka@gmail.com>
2022-01-16 17:33:12 +0100

    helper method for business logic

--- src/dataservercontroller/requiredServer.ts
+++ src/dataservercontroller/requiredServer.ts
@@ -12,3 +12,30 @@ interface IPlannerServer {
     virtualMachine:Array<serverDefinition>
   ): number;
 }
+
+class Planner implements IPlannerServer {
+  serverType;
+  virtualMachine;
+  data: Array<serverDefinition> = [];
+  capacity: number = 0;
+
+  constructor(serverType:serverDefinition, virtualMachine:Array<serverDefinition>) {
+    this.serverType = serverType;
+    this.virtualMachine = virtualMachine;
+  }
+
+  calculatePlanner(): number {
+    let initialServerType: serverDefinition = {
+      CPU: 0,
+      RAM: 0,
+      HDD: 0,
+    };
+    // sorting virtual machines
+    this.virtualMachine.sort((x,y) => {
+      let costForX = x.CPU + x.RAM + x.HDD;
+      let costForY = y.CPU + y.HDD + y.HDD
+
+      return costForX - costForY
+    })
+  }
+}
