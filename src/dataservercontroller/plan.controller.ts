6484054d8e57adca99e64266e2260c91ed4b27bf
chi <chinedu.onyeoka@gmail.com>
2022-01-16 19:32:22 +0100

    plan controller

--- src/dataservercontroller/plan.controller.ts
+++ src/dataservercontroller/plan.controller.ts
@@ -1,5 +1,5 @@
 // import { Controller, Post, Get } from  '@nestjs/common'
-import { Request, Response, NextFunction,}
+import { Request, Response, NextFunction} from 'express'
 import Planner from "../CommonMethods/requiredServer"
 
 // @Controller('server')
@@ -7,6 +7,24 @@ export default class PlanController {
 
 
   public static async calculatePlanner(
+    req: Request,
+    res: Response,
+    next: NextFunction
+      ) {
+        try {
+          const { serverType, virtualMachine} = req.body;
+          const vmController = new Planner(serverType, virtualMachine);
+          const result = vmController.calculatePlanner();
 
-  )
+          return  res.status(200).send({
+            message:
+            "server type capacity successful",
+            result: {
+              value: result,
+            }
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
 }
